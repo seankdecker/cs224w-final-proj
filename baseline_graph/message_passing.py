909952e5f81aebe08a691deb1342be1f4c633961
seankdecker <seankdecker@gmail.com>
2019-12-10 14:22:55 -0800

    adding subgraph gen code

--- baseline_graph/message_passing.py
+++ baseline_graph/message_passing.py
@@ -1,9 +1,14 @@
-import load_banned, load_graph, get_banned_ids from load_datasets
+from load_datasets import load_banned
+from load_datasets import load_graph
+from load_datasets import get_banned_ids, banned_sub_in_G
 import random
 DATASET = 'title' 
 
-def get_random_subgraph(G, banned, subreddit_to_id):
-  banned_ids = get_banned_ids(banned, subreddit_to_id)
+subgraph_size = 300
+
+def get_random_subgraph(G, banned_ids):
+  banned_ids = random.sample(banned_ids, 10)
+  others_ids = random.sample()
   
 
 TESTING, TRAINING = [], []
@@ -12,5 +17,7 @@ if __name__ == '__main__':
   G, id_to_subreddit, subreddit_to_id = load_graph(DATASET)
   banned = load_banned()
   banned = banned_sub_in_G(G, id_to_subreddit, banned)
-  TRAINING = random.sample(banned, len(banned) * 3 / 4)
+  TRAINING = random.sample(banned, int(len(banned) * 3 / 4))
+  TESTING = list(set(banned) - set(TRAINING))
+  print(TRAINING, TESTING)
 
